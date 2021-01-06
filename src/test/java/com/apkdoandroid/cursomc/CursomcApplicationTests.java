c7e15ce5640d7c704ff85e70a6ffd7b07e496d8b
Elanilson <elanilsonpp@hotmail.com>
2021-01-06 13:38:12 -0300

    projeto criado

---
+++ src/test/java/com/apkdoandroid/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apkdoandroid.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
